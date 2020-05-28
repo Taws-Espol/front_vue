42e9aef0a49cdbbe01eaee806506f8f18f4ff925
Piero512 <ulloapiero@gmail.com>
2020-05-28 17:16:21 -0500

    Mover los callbacks a methods en Vue.js

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -2,27 +2,37 @@
     <div>
         <!-- Interpolación de texto -->
         <h2>{{myText}}</h2>
-        <button v-on:click="shouldShow = !shouldShow">
-            Cambiar visibilidad</button>
-        <!-- Ejemplos de v-if y v-else -->
-        <div v-if="shouldShow">
-            Now you see me
+        <!-- v-model 2 way data binding -->
+        <div>
+            <label for="texto">Texto a mostrar: </label>
+            <input id="texto" v-model="myText"/>
         </div>
-        <div v-else>
-            Now you don't
+        <div>
+            <button v-on:click="shouldShow = !shouldShow">
+                Cambiar visibilidad</button>
+            <!-- Ejemplos de v-if y v-else -->
+            <h2 v-if="shouldShow">
+            Ahora me ves
+            </h2>
+            <h2 v-else>
+                Ahora no me ves
+            </h2>
         </div>
-        <button v-on:click="listItems.push(listItems.length + 1)">
+        <div class="btn-holder">
+            <button v-on:click="addItem">
             Añadir un número a la lista
         </button>
-        <button v-on:click="listItems.pop()">
+        <button v-on:click="removeItem">
             Remover un número a la lista
         </button>
-        <ul>
-           <li v-for="item in listItems" :key="item">
-               <h2>{{item}}</h2>
-           </li>
-        </ul>
-        
+        </div>
+        <div class="shorter">
+            <ul>
+                <li v-for="item in listItems" :key="item">
+                    {{item}}
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -31,20 +41,46 @@ export default {
     data: function () 
     {
         return {
-            myText: "Texto de ejemplo",
-            shouldShow: true,
+            myText: "Edita el cuadro de abajo para cambiar el texto!",
+            shouldShow: false,
             listItems: [
                 1,2,3,4
             ]
+        }
+    },
+    methods: {
+        addItem: function() {
+            this.listItems.push(this.listItems.length + 1)
+        },
+        removeItem: function(){
+            this.listItems.pop()
         }
     }
 
 }
 </script>
 <style scoped>
+template{
+    box-sizing: border-box;
+}
 h2{
     color: green;
 }
+
+div {
+    padding: 5px;
+}
+
+.btn-holder {
+    display: flex;
+    justify-content: space-evenly;
+
+}
+
+.shorter {
+    width: 20%;
+}
+
 
 </style>
 
