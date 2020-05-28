6a2b2f74e9039cd9d12175bc7ff35023c057d48f
Piero512 <ulloapiero@gmail.com>
2020-05-28 14:00:38 -0500

    Mi primer componente

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h2>{{myText}}</h2>
+        <button v-on:click="shouldShow = !shouldShow">
+            Cambiar visibilidad</button>
+        <!-- Ejemplos de v-if y v-else -->
+        <div v-if="shouldShow">
+            Now you see me
+        </div>
+        <div v-else>
+            Now you don't
+        </div>
+        <button v-on:click="listItems.push(listItems.length + 1)">
+            Añadir un número a la lista
+        </button>
+        <button v-on:click="listItems.pop()">
+            Remover un número a la lista
+        </button>
+        <ul>
+           <li v-for="item in listItems" :key="item">
+               <h2>{{item}}</h2>
+           </li>
+        </ul>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () 
+    {
+        return {
+            myText: "Texto de ejemplo",
+            shouldShow: true,
+            listItems: [
+                1,2,3,4
+            ]
+        }
+    }
+
+}
+</script>
+<style scoped>
+h2{
+    color: green;
+}
+
+</style>
+
