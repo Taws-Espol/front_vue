7fb5b2034d547488eda574e69c7f97e45e9df3f4
Piero512 <ulloapiero@gmail.com>
2020-06-05 10:47:16 -0500

    Add some sample data and utility functions.

--- src/components/ShoppingCart/ShoppingPanel.vue
+++ src/components/ShoppingCart/ShoppingPanel.vue
@@ -6,7 +6,78 @@
 
 <script>
     export default {
-        name: "ShoppingPanel"
+        name: "ShoppingPanel",
+        data: function () {
+            return {
+                products: [
+                    {
+                        name: "Peras",
+                        price: 2.54,
+                        photo: './Single_apple.png',
+                        stock: 20
+                    },
+                    {
+                        name: "Manzanas",
+                        price: 2.54,
+                        photo: './Single_apple.png',
+                        stock: 20
+                    },
+                    {
+                        name: "Papayas",
+                        price: 2.54,
+                        photo: './Single_apple.png',
+                        stock: 20
+                    },
+                    {
+                        name: "Aguacates",
+                        price: 2.54,
+                        photo: './Single_apple.png',
+                        stock: 20
+                    },
+                    {
+                        name: "Bananas",
+                        price: 2.54,
+                        photo: './Single_apple.png',
+                        stock: 20
+                    },
+                    {
+                        name: "Verdes",
+                        price: 2.54,
+                        photo: './Single_apple.png',
+                        stock: 20
+                    },
+                    {
+                        name: "Maduros",
+                        price: 2.54,
+                        photo: './Single_apple.png',
+                        stock: 20
+                    },
+                    {
+                        name: "Piñas",
+                        price: 2.54,
+                        photo: './Single_apple.png',
+                        stock: 20
+                    },
+                ],
+                userCart: [
+
+                ],
+            }
+        },
+        methods: {
+            sortProductsByPrice: function(descending = false){
+                if (descending) {
+                    this.products.sort((a,b) => b.price - a.price);
+                } else {
+                    this.products.sort((a, b) => a.price - b.price);
+                }
+            }
+        },
+        computed: {
+            getSearchTerm: function(term) {
+                return this.products.filter((product) => product.name.includes(term));
+            },
+        }
     }
 </script>
 
