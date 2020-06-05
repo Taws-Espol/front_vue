5d46f93bc76d5769f7fae7aa563308b9fe72cac4
Piero512 <ulloapiero@gmail.com>
2020-06-05 11:50:34 -0500

    Add shopping panel widget

---
+++ src/components/ShoppingCart/ShoppingItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="card">
+            <img class="card-img-top" v-bind:src="item.photo">
+            <div class="card-body">
+                <h5 class="card-title">{{item.name}}</h5>
+                <p class="card-text">Stock: {{item.stock}}</p>
+                <p class="card-text">Precio: ${{item.price.toFixed(2)}}</p>
+                <div class="btn-group">
+                    <button class="btn btn-primary">Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ShoppingItem",
+        props: {
+            item: Object
+        },
+        methods: {
+            addToCart: function(){
+                this.$emit('addToCart', this.item);
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+    img {
+        width: 200px;
+        height: 200px;
+    }
+</style>
--- src/components/ShoppingCart/ShoppingPanel.vue
+++ src/components/ShoppingCart/ShoppingPanel.vue
@@ -1,61 +1,85 @@
 <template>
-    <div class="container">
-        <h2>TODO: ShoppingPanel</h2>
+    <div class="container-fluid">
+        <div class="container" >
+            <h2>Items en stock</h2>
+            <button class="btn btn-dark" v-on:click="toggleSortingOrder">Ordenar por precio</button>
+            <div class="row">
+                <shopping-item
+                        style="padding-top: 10px;"
+                        class="col-4"
+                        v-bind:item="product"
+                        v-for="product in products" :key="product.id"
+                ></shopping-item>
+
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
+    import ShoppingItem from "./ShoppingItem";
     export default {
         name: "ShoppingPanel",
+        components: {ShoppingItem},
         data: function () {
             return {
+                descending: false,
                 products: [
                     {
+                        id: 1,
                         name: "Peras",
                         price: 2.57,
-                        photo: './Single_apple.png',
+                        photo: './pear.png',
                         stock: 20
                     },
                     {
+                        id: 2,
                         name: "Manzanas",
                         price: 1.83,
                         photo: './Single_apple.png',
                         stock: 20
                     },
                     {
+                        id: 3,
                         name: "Papayas",
                         price: 2.54,
-                        photo: './Single_apple.png',
+                        photo: './papaya.png',
                         stock: 20
                     },
                     {
+                        id: 4,
                         name: "Aguacates",
                         price: 3.54,
-                        photo: './Single_apple.png',
+                        photo: './aguacate.png',
                         stock: 20
                     },
                     {
+                        id: 5,
                         name: "Bananas",
                         price: 1.25,
-                        photo: './Single_apple.png',
+                        photo: './bananas.png',
                         stock: 20
                     },
                     {
+                        id: 6,
                         name: "Verdes",
                         price: 1.85,
-                        photo: './Single_apple.png',
+                        photo: './verde.png',
                         stock: 20
                     },
                     {
+                        id: 7,
                         name: "Maduros",
                         price: 1.50,
-                        photo: './Single_apple.png',
+                        photo: './bananas.png',
                         stock: 20
                     },
                     {
+                        id: 8,
                         name: "Piñas",
                         price: 3.50,
-                        photo: './Single_apple.png',
+                        photo: './pina.png',
                         stock: 20
                     },
                 ],
@@ -65,6 +89,10 @@
             }
         },
         methods: {
+            toggleSortingOrder: function(){
+                this.descending = !this.descending;
+                this.sortProductsByPrice(this.descending);
+            },
             sortProductsByPrice: function(descending = false){
                 if (descending) {
                     this.products.sort((a,b) => b.price - a.price);
